6bd0be86ef61afa72b35f0bcef3cff8d16f214bd
Sara El Badry <sara2lbadry@gmail.com>
2022-07-14 15:23:19 +0200

    color generator structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from "react";
+import SingleColor from './SingleColor'
+import Values from 'values.js'
 function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+const handleSubmit = (e)=> {
+  e.preventDefault();
+  
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+      <h2>color generator</h2>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value = {color} onChange = {(e) => setColor(e.target.value)} placeholder="#ffffff"/>
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </section>
+    
   );
 }
 
