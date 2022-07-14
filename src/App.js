c20eed368c23016a4f814cef1a163d07d6eaf2aa
Sara El Badry <sara2lbadry@gmail.com>
2022-07-14 16:06:36 +0200

    display color shades

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const handleSubmit = (e)=> {
   e.preventDefault();
   try {
     let colors = new Values(color).all(10)
-    console.log(colors)
+    setList(colors)
     
   } catch (error) {
     setError(true)
@@ -21,16 +21,26 @@ const handleSubmit = (e)=> {
   
 }
   return (
-    <section className="container">
-      <h2>color generator</h2>
-      <form onSubmit={handleSubmit}>
-        <input type='text' value = {color} onChange = {(e) => setColor(e.target.value)} placeholder="#ffffff"
-        className= {`${error? 'error':null}`} />
-        <button className="btn" type="submit">
-          Submit
-        </button>
-      </form>
-    </section>
+    <>
+      <section className="container">
+        <h2>color generator</h2>
+        <form onSubmit={handleSubmit}>
+          <input type='text' value = {color} onChange = {(e) => setColor(e.target.value)} placeholder="#ffffff"
+          className= {`${error? 'error':null}`} />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+
+      <section className="colors">
+        {list.map((color,index) => {
+          return<SingleColor key={index} {...color} index = {index} hexColor = {color.hex}/>
+        })}
+
+      </section>
+    </>
+    
     
   );
 }
